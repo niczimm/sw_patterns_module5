48f3e77df2250081a5910768013eb8e863140ebf
Jane Doe <nicolelzimmerer@ou.edu>
2025-10-03 15:53:14 -0500

    singleton with builder

---
+++ build/CMakeFiles/Patterns.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nicolezimmerer/JH/sw_patterns/Mod5/src/Attr.C" "CMakeFiles/Patterns.dir/Attr.C.o" "gcc" "CMakeFiles/Patterns.dir/Attr.C.o.d"
+  "/Users/nicolezimmerer/JH/sw_patterns/Mod5/src/Builder.C" "CMakeFiles/Patterns.dir/Builder.C.o" "gcc" "CMakeFiles/Patterns.dir/Builder.C.o.d"
+  "/Users/nicolezimmerer/JH/sw_patterns/Mod5/src/Director.C" "CMakeFiles/Patterns.dir/Director.C.o" "gcc" "CMakeFiles/Patterns.dir/Director.C.o.d"
+  "/Users/nicolezimmerer/JH/sw_patterns/Mod5/src/Document.C" "CMakeFiles/Patterns.dir/Document.C.o" "gcc" "CMakeFiles/Patterns.dir/Document.C.o.d"
+  "/Users/nicolezimmerer/JH/sw_patterns/Mod5/src/Element.C" "CMakeFiles/Patterns.dir/Element.C.o" "gcc" "CMakeFiles/Patterns.dir/Element.C.o.d"
+  "/Users/nicolezimmerer/JH/sw_patterns/Mod5/src/NamedNodeMap.C" "CMakeFiles/Patterns.dir/NamedNodeMap.C.o" "gcc" "CMakeFiles/Patterns.dir/NamedNodeMap.C.o.d"
+  "/Users/nicolezimmerer/JH/sw_patterns/Mod5/src/Node.C" "CMakeFiles/Patterns.dir/Node.C.o" "gcc" "CMakeFiles/Patterns.dir/Node.C.o.d"
+  "/Users/nicolezimmerer/JH/sw_patterns/Mod5/src/Test.C" "CMakeFiles/Patterns.dir/Test.C.o" "gcc" "CMakeFiles/Patterns.dir/Test.C.o.d"
+  "/Users/nicolezimmerer/JH/sw_patterns/Mod5/src/Text.C" "CMakeFiles/Patterns.dir/Text.C.o" "gcc" "CMakeFiles/Patterns.dir/Text.C.o.d"
+  "/Users/nicolezimmerer/JH/sw_patterns/Mod5/src/ValidChildren.C" "CMakeFiles/Patterns.dir/ValidChildren.C.o" "gcc" "CMakeFiles/Patterns.dir/ValidChildren.C.o.d"
+  "/Users/nicolezimmerer/JH/sw_patterns/Mod5/src/XMLSerializer.C" "CMakeFiles/Patterns.dir/XMLSerializer.C.o" "gcc" "CMakeFiles/Patterns.dir/XMLSerializer.C.o.d"
+  "/Users/nicolezimmerer/JH/sw_patterns/Mod5/src/XMLTokenizer.C" "CMakeFiles/Patterns.dir/XMLTokenizer.C.o" "gcc" "CMakeFiles/Patterns.dir/XMLTokenizer.C.o.d"
+  "/Users/nicolezimmerer/JH/sw_patterns/Mod5/src/XMLValidator.C" "CMakeFiles/Patterns.dir/XMLValidator.C.o" "gcc" "CMakeFiles/Patterns.dir/XMLValidator.C.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Patterns.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Patterns.dir/Attr.C.o"
+  "CMakeFiles/Patterns.dir/Attr.C.o.d"
+  "CMakeFiles/Patterns.dir/Builder.C.o"
+  "CMakeFiles/Patterns.dir/Builder.C.o.d"
+  "CMakeFiles/Patterns.dir/Director.C.o"
+  "CMakeFiles/Patterns.dir/Director.C.o.d"
+  "CMakeFiles/Patterns.dir/Document.C.o"
+  "CMakeFiles/Patterns.dir/Document.C.o.d"
+  "CMakeFiles/Patterns.dir/Element.C.o"
+  "CMakeFiles/Patterns.dir/Element.C.o.d"
+  "CMakeFiles/Patterns.dir/NamedNodeMap.C.o"
+  "CMakeFiles/Patterns.dir/NamedNodeMap.C.o.d"
+  "CMakeFiles/Patterns.dir/Node.C.o"
+  "CMakeFiles/Patterns.dir/Node.C.o.d"
+  "CMakeFiles/Patterns.dir/Test.C.o"
+  "CMakeFiles/Patterns.dir/Test.C.o.d"
+  "CMakeFiles/Patterns.dir/Text.C.o"
+  "CMakeFiles/Patterns.dir/Text.C.o.d"
+  "CMakeFiles/Patterns.dir/ValidChildren.C.o"
+  "CMakeFiles/Patterns.dir/ValidChildren.C.o.d"
+  "CMakeFiles/Patterns.dir/XMLSerializer.C.o"
+  "CMakeFiles/Patterns.dir/XMLSerializer.C.o.d"
+  "CMakeFiles/Patterns.dir/XMLTokenizer.C.o"
+  "CMakeFiles/Patterns.dir/XMLTokenizer.C.o.d"
+  "CMakeFiles/Patterns.dir/XMLValidator.C.o"
+  "CMakeFiles/Patterns.dir/XMLValidator.C.o.d"
+  "Patterns"
+  "Patterns.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Patterns.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
